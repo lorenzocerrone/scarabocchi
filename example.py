606353807fdd965ea973a3930c68b46544f90bfd
lorenzocerrone <lorenzo.cerrone@iwr.uni-heidelberg.de>
2019-06-04 10:56:26 +0200

    simple example

---
+++ example.py
@@ -0,0 +1,5 @@
+from scarabocchi.scarabocchi import scribbles_tools2d
+from skimage.data import astronaut
+
+# Simple annotator example
+annotator = scribbles_tools2d(img=astronaut(), figsize=(10, 10))
